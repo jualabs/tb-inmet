cf76d4bf85547b1e4b10a272766661bf367553c1
victormedeiros <victorwcm@gmail.com>
2018-04-30 17:10:43 -0300

    delete test files

--- tb_inmet_utils.py
+++ tb_inmet_utils.py
@@ -4,6 +4,7 @@ import requests
 import json
 import swagger_client
 
+
 def get_api_tokens_from_password(hostname, username, password):
     headers = {
         'Content-Type': 'application/json',
@@ -17,39 +18,24 @@ def get_api_tokens_from_password(hostname, username, password):
     tokens = json.loads(response.content)
     return tokens
 
-def get_api_tokens_from_refresh_token(hostname, tokens):
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
-
-    data =  '{"grantType":"REFRESH_TOKEN","access_token":"' + tokens['token'] + '","refresh_token":"' + tokens['refreshToken'] + '"}'
-
-    response = requests.post('http://' + hostname.strip() + '/api/auth/token', headers=headers, data=data)
-
-    tokens = json.loads(response.content)
-    return tokens
 
 def renew_token(configuration):
     tokens = get_api_tokens_from_password(configuration.host, configuration.username, configuration.password)
     configuration.api_key['X-Authorization'] = tokens['token']
 
-def get_api_configuration(hostname='', username='', password=''):
-    # get hostname, username and password from user
-    if(hostname == ''):
-        hostname = raw_input("please enter hostname: ")
-    if (username == ''):
-        username = raw_input("please enter username: ")
-    if (password == ''):
-        password = raw_input("please enter password: ")
-    # get tokens
-    tokens = get_api_tokens_from_password(hostname, username, password)
-    # configure API key authorization: X-Authorization
-    configuration = swagger_client.Configuration()
-    configuration.host = hostname
-    configuration.username = username
-    configuration.password = password
-    configuration.api_key['X-Authorization'] = tokens['token']
-    configuration.api_key_prefix['X-Authorization'] = 'Bearer'
 
-    return configuration
+def get_tb_api_configuration(cfg_params):
+    # set configurations values
+    # set hostname, username and password from user
+    api_cfg = swagger_client.Configuration()
+    api_cfg.host = cfg_params['tb_api_access']['host']
+    api_cfg.username = cfg_params['tb_api_access']['user']
+    api_cfg.password = cfg_params['tb_api_access']['passwd']
+
+    # get tokens
+    tokens = get_api_tokens_from_password(api_cfg.host, api_cfg.username, api_cfg.password)
+    # configure API key authorization: X-Authorization
+    api_cfg.api_key['X-Authorization'] = tokens['token']
+    api_cfg.api_key_prefix['X-Authorization'] = 'Bearer'
+
+    return api_cfg
